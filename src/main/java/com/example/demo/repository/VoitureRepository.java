048ff000c367b1426bafcd165e0159fc542d44e3
Neel-Leo Coffin <neelleo.coffin.etu@univ-lille.fr>
2020-11-09 13:41:44 +0100

    remove unapropriate upper case

--- src/main/java/com/example/demo/repository/VoitureRepository.java
+++ src/main/java/com/example/demo/repository/VoitureRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface VoitureRepository extends JpaRepository<VoitureEntity, Integer> {
-    @Query(value = "select * from Voiture", nativeQuery = true)
+    @Query(value = "select * from voiture", nativeQuery = true)
     Iterable<VoitureEntity> findAllVoiture();
 }
